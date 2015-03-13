232bedd97fae028a56d233baa0924f9502f74a38
saasbook <saasbook@saasbook.(none)>
2015-03-13 13:13:07 -0300

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,37 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @sort = nil
+    @ratings = @all_ratings
+
+    if params.fetch("ratings", false)
+      @ratings = params[:ratings].keys
+    end
+
+    if ["title", "release_date"].member?(params.fetch("sort", nil))
+      @sort = params[:sort]
+    end
+
+    sess_sort = session.fetch("movies_sort", nil)
+    sess_ratings = session.fetch("movies_ratings", @all_ratings)
+
+    # If you access default URL and have stored settings, then redirect you to right URL
+    if (@ratings == @all_ratings and @sort == nil and
+        (sess_ratings != @all_ratings or sess_sort != nil))
+      flash.keep
+      # We store ratings as array, but to GET params they should pass as hash
+      ratings_params = Hash[*sess_ratings.collect {|k| [k, 'yes']}.flatten]
+      return redirect_to movies_path(nil, {:sort => sess_sort, :ratings => ratings_params})
+    end
+
+    @movies = Movie.where(:rating => @ratings)
+    if @sort != nil
+      @movies = @movies.order(@sort)
+    end
+
+    session[:movies_sort] = @sort
+    session[:movies_ratings] = @ratings
   end
 
   def new
